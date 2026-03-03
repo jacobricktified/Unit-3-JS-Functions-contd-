23eaa70476cdd63fac9888eaa75bfe14403e61f4
jacob gichira mbuco <mbucojacob@gmail.com>
2026-03-03 19:36:27 +0300

    Use functions to perform operations.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+//function with default activity of roller skating 
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}   
+//function with default activity of go to the office
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+} 
+//function that takes a string as an argument and returns a function that wraps an adjective in a highlight
+function wrapAdjective(highlight) {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    }
+}
+  
+
